d9620fb735c4deccaf0eef76802df902edf00bf6
trenpetrov-ops <tren.petrov@gmail.com>
2025-09-19 09:38:45 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,3 +9,12 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("✅ Service Worker registered"))
+      .catch((err) => console.log("❌ SW registration failed:", err));
+  });
+} 
